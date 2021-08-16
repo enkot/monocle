e2df4fe967a8d170e82093c79850326558a46002
Enkot <taras.batenkov@gmail.com>
2021-08-16 12:17:33 +0300

    add vercel.json

--- api/hello.ts
+++ api/hello.ts
@@ -3,14 +3,14 @@ import { v4 } from 'uuid'
 
 const app = express()
 
-app.get('/api', (req: any, res: any) => {
+app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
 })
 
-app.get('/api/item/:slug', (req: any, res: any) => {
+app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params
   res.end(`Item: ${slug}`)
 })
